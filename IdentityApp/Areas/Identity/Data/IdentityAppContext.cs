ded82c3f69fb85c48c5714142a5b16b26714e666
bonnina <37539513+bonnina@users.noreply.github.com>
2019-04-24 07:39:35 +0300

    Add custom navigation properties (draft)

--- IdentityApp/Areas/Identity/Data/IdentityAppContext.cs
+++ IdentityApp/Areas/Identity/Data/IdentityAppContext.cs
@@ -9,7 +9,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace IdentityApp.Models
 {
-    public class IdentityAppContext : IdentityDbContext<IdentityAppUser>
+    public class IdentityAppContext 
+        : IdentityDbContext<IdentityAppUser
+            //, ApplicationRole, string,
+            //ApplicationUserClaim, ApplicationUserRole, ApplicationUserLogin,
+            //ApplicationRoleClaim, ApplicationUserToken
+            >
     {
         public IdentityAppContext(DbContextOptions<IdentityAppContext> options)
             : base(options)
@@ -19,9 +24,42 @@ namespace IdentityApp.Models
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
+
+            //{
+            //    // Each User can have many UserClaims
+            //    b.HasMany(e => e.Claims)
+            //        .WithOne()
+            //        .HasForeignKey(uc => uc.UserId)
+            //        .IsRequired();
+            
+            //    b.HasMany(e => e.Logins)
+            //        .WithOne()
+            //        .HasForeignKey(ul => ul.UserId)
+            //        .IsRequired();
+            
+            //    b.HasMany(e => e.Tokens)
+            //        .WithOne()
+            //        .HasForeignKey(ut => ut.UserId)
+            //        .IsRequired();
+            
+            //    b.HasMany(e => e.UserRoles)
+            //        .WithOne()
+            //        .HasForeignKey(ur => ur.UserId)
+            //        .IsRequired();
+            //});
+
+            //builder.Entity<ApplicationRole>(b =>
+            //{
+            //    b.HasMany(e => e.UserRoles)
+            //        .WithOne(e => e.Role)
+            //        .HasForeignKey(ur => ur.RoleId)
+            //        .IsRequired();
+
+            //    b.HasMany(e => e.RoleClaims)
+            //        .WithOne(e => e.Role)
+            //        .HasForeignKey(rc => rc.RoleId)
+            //        .IsRequired();
+            //});
         }
     }
 }
--- IdentityApp/Areas/Identity/Data/IdentityAppUser.cs
+++ IdentityApp/Areas/Identity/Data/IdentityAppUser.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Identity;
 
 namespace IdentityApp.Areas.Identity.Data
 {
-    // Add profile data for application users by adding properties to the IdentityAppUser class
     public class IdentityAppUser : IdentityUser
     {
         [PersonalData]
@@ -14,5 +13,42 @@ namespace IdentityApp.Areas.Identity.Data
 
         [PersonalData]
         public DateTime DOB { get; set; }
+
+        //public virtual ICollection<IdentityUserClaim<string>> Claims { get; set; }
+        //public virtual ICollection<IdentityUserLogin<string>> Logins { get; set; }
+        //public virtual ICollection<IdentityUserToken<string>> Tokens { get; set; }
+        //public virtual ICollection<IdentityUserRole<string>> UserRoles { get; set; }
     }
+
+    //public class ApplicationRole : IdentityRole
+    //{
+    //    public virtual ICollection<ApplicationUserRole> UserRoles { get; set; }
+    //    public virtual ICollection<ApplicationRoleClaim> RoleClaims { get; set; }
+    //}
+
+    //public class ApplicationUserRole : IdentityUserRole<string>
+    //{
+    //    public virtual IdentityAppUser User { get; set; }
+    //    public virtual ApplicationRole Role { get; set; }
+    //}
+
+    //public class ApplicationUserClaim : IdentityUserClaim<string>
+    //{
+    //    public virtual IdentityAppUser User { get; set; }
+    //}
+
+    //public class ApplicationUserLogin : IdentityUserLogin<string>
+    //{
+    //    public virtual IdentityAppUser User { get; set; }
+    //}
+
+    //public class ApplicationRoleClaim : IdentityRoleClaim<string>
+    //{
+    //    public virtual ApplicationRole Role { get; set; }
+    //}
+
+    //public class ApplicationUserToken : IdentityUserToken<string>
+    //{
+    //    public virtual IdentityAppUser User { get; set; }
+    //}
 }
